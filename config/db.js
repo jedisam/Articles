645345458587fc6ecaa3378b1a8e938244ac9c07
Yididya Samuel <yidisam18@gmail.com>
2020-04-29 09:40:39 -0400

    Db accesss

---
+++ config/db.js
@@ -0,0 +1,6 @@
+module.exports = {
+    // database:'mongodb://localhost:27017/Articles',
+    database:'mongodb+srv://yidisam123:yidisam123@cluster0-ht8pf.mongodb.net/test?retryWrites=true&w=majority',
+    secret:'mySecret'
+}
+
